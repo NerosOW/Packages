cb616d9365d062fe27014cd4f1617df3bc2efa77
Daniel Gonzalez <77129792+NerosOW@users.noreply.github.com>
2025-08-31 20:38:37 -0400

    Modified by www.SourceFiles.app

---
+++ master@homebrew-ioszip/ioszip.rb
@@ -0,0 +1,15 @@
+require "formula"
+
+class Ioszip < Formula
+  homepage "https://github.com/ioszip/ioszip.github.io"
+  url "https://github.com/ioszip/ioszip.github.io/archive/1.0.0.zip"
+  sha1 "d89f87513d03cead071a64e09e3a809e548ab2f3"
+
+  skip_clean 'bin'
+
+  def install
+    prefix.install 'bin'
+    bin.chmod 0755
+  end
+
+end
